075dc7b0c5766c0b7ce66bfd09e45216df974efc
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-06-01 18:10:15 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задание 3. Совместная работа
+
+// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+
+// Указание:
+// Использовать рекурсию. Не использовать цикл.
+
+// Пример:
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+Console.WriteLine("Введите строку из латинских букв: ");
+string text = Console.ReadLine();
+Console.WriteLine();
+
+PrintConsonants(text);
+
+void PrintConsonants(string str, int index = 0)
+{
+    if (index == str.Length)
+    {
+        return;
+    }
+
+    string vowels = "aeiouy";
+    if (char.IsAsciiLetter(str[index]) && !vowels.Contains(str[index]))
+    {
+        Console.Write($"{str[index]}");
+    }
+    PrintConsonants(str, index + 1);
+}
