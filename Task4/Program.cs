b2b10e75951ff87b187d2dfbcef2ee12db514ae1
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-06-02 16:17:48 +0300

    Added info in Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,27 +8,46 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
+int ReadNaturalNumber()
+{
+    int number;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out number) && number > 0)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Введите натуральное число: ");
+        }
+    }
+}
+
 void PrintNaturalNumbers(int M, int N)
 {
-    if (M > N) return;
-    Console.Write($"{M} ");
-    PrintNaturalNumbers(M + 1, N);
+    if (M == N)
+    {
+        Console.Write($"{M}");
+    }
+    else
+    {
+        Console.Write($"{M}, ");
+        if (M < N)
+        {
+            PrintNaturalNumbers(M + 1, N);
+        }
+        else
+        {
+            PrintNaturalNumbers(M - 1, N);
+        }
+    }
 }
 
 Console.WriteLine("Введите начальное значение M: ");
-int M = Convert.ToInt32(Console.ReadLine());
+int M = ReadNaturalNumber();
 
 Console.WriteLine("Введите конечное значение N: ");
-int N = Convert.ToInt32(Console.ReadLine());
+int N = ReadNaturalNumber();
 
 PrintNaturalNumbers(M, N);
-
-
-// 1. Метод PrintNaturalNumbers(int M, int N) принимает два аргумента:
-// начальное значение M и конечное значение N.
-
-// 2. Если значение M больше N, рекурсия прекращается.
-
-// 3. В противном случае текущее значение M выводится на консоль,
-// после чего вызывается рекурсивный метод PrintNaturalNumbers 
-// с увеличеним значения M на 1.
